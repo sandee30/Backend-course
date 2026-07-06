171a4fb3915f89f51fd85375bb77d4bcc7f84382
sandee30 <sandeep.of.30@gmail.com>
2026-07-07 00:37:54 +0545

    post and get api

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
-const express = require('express')
+const app = require('./src/app')
 
-const app = express()
 
-app.get("/",(req,res)=>{
-    res.send("Hello world")
+app.listen(3000,()=>{
+    console.log("server is running at port 3000");
+    
 })
-
-app.get("/about",(req,res)=>{
-    res.send("this is about page.")
-})
-
-app.listen(3000)
