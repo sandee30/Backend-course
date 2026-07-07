5d6d92f9b039743156aa245886f67acc8270c390
sandee30 <sandeep.of.30@gmail.com>
2026-07-07 07:42:18 +0545

    delete api

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+// server.js file is only for running the server
+
 const app = require('./src/app')
 
 
 app.listen(3000,()=>{
-    console.log("server is running at port 3000");
+    console.log("Server is running on 3000");
     
 })
+
+
--- src/app.js
+++ src/app.js
@@ -2,11 +2,38 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
 
-app.get("/",(req,res)=>{
-    res.send("hello world")
+const notes = []
+
+app.post("/notes",(req,res)=>{
+    notes.push(req.body)
+    res.status(201).json({
+        message: "note created successfully"
+    })
+})
+
+app.get("/notes",(req,res)=>{
+
+    
+    
+    res.status(200).json({
+        message: "note read successfully.",
+        notes: notes
+    })
 })
 
 
+app.delete('/notes/:index',(req,res)=>{
+
+    const index = req.params.index
+    delete notes [index]
+    res.status(200).json({
+        message: "note deleted successfully."
+    })
+})
 
 module.exports = app
+
+
+
